52298cfab576f74afdcb272c042e3d55323c5d8f
Jack Wang(王杰) <wangjie2017@inspur.com>
2017-12-15 11:16:32 +0800

    d

--- es_example/src/main/java/com/inspur/bigdata/manage/es/zsjexample/ESUtils.java
+++ es_example/src/main/java/com/inspur/bigdata/manage/es/zsjexample/ESUtils.java
@@ -4,11 +4,9 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 import org.elasticsearch.action.admin.cluster.state.ClusterStateResponse;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
@@ -17,11 +15,17 @@ import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
 import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsResponse;
 import org.elasticsearch.action.admin.indices.mapping.get.GetMappingsRequest;
 import org.elasticsearch.action.admin.indices.mapping.get.GetMappingsResponse;
+import org.elasticsearch.action.bulk.BulkProcessor;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexRequestBuilder;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateRequestBuilder;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.IndicesAdminClient;
 import org.elasticsearch.client.transport.TransportClient;
@@ -29,33 +33,60 @@ import org.elasticsearch.cluster.metadata.MappingMetaData;
 import org.elasticsearch.common.collect.ImmutableOpenMap;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.unit.ByteSizeUnit;
+import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.reindex.BulkByScrollResponse;
+import org.elasticsearch.index.reindex.DeleteByQueryAction;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.carrotsearch.hppc.cursors.ObjectObjectCursor;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 /**
- * elasticsearch操作工具类
+ * elasticsearch operation tool
+ * 
+ * all operation has two styles
+ * 
+ * one way oby restfull strictly,use index(update\delete)Request,and client call
+ * index(update\delete)method to exec request,just like httclient to exec
+ * HttpGet,HttpPost,HttpDelete.
+ * 
+ * another way oby restfull not strictly,use less code like this,
+ * client.prepareIndex...(prepareUpdate,prepareDeleter) and return response
+ * 
+ * 
+ * when you write to index,if index not exist,it will create one ;if exist then
+ * do as nomal
+ * 
+ * when you get many request,you should ues bulkProcess or BulkRequestBuilder
  * 
  */
 
 public class ESUtils {
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
+
+		TransportClient client = getClient();
 		// createIndex(getClient("es", "10.110.13.176"), "ql3", "ql3", 5, 3);
-		// writeDocumentByPrepareIndex(getClient(), "tohdfs", "tohdfs");
+		// writeDocumentByPrepareIndex(client, "tohdfs", "tohdfs","001");
 
-		// writeDocumentByIndexRequest(getClient(), "tohdfs", "tohdfs");
+		// writeDocumentByIndexRequest(client, "tohdfs", "tohdfs","0");
 
-		UpdateDocumentByPrepareUpdate(getClient(), "tohdfs", "tohdfs");
-		// writeDocument
+		// UpdateDocumentByPrepareUpdate(client, "tohdfs", "tohdfs","0");
+
+		// deleteDocumentByPrepareDelete(client, "tohdfs", "tohdfs","0");
+		// deleteDocumentByDeleteRequest(client, "tohdfs", "tohdfs", "3");
+		// useBulkRequestBuilder(client, "tohdfs", "tohdfs");
+		// useBulkProcessor(client, "tohdfs", "tohdfs");
+		deleteDocumentByQuery(client, "tohdfs");
 	}
 
 	/****
@@ -164,20 +195,20 @@ public class ESUtils {
 	 * 
 	 * @throws IOException
 	 */
-	public static void writeDocumentByPrepareIndex(TransportClient client, String indexName, String typeName)
+	public static void writeDocumentByPrepareIndex(TransportClient client, String indexName, String typeName, String id)
 			throws IOException {
 
 		Map<String, String> parm = new HashMap<String, String>(1);
 
-		parm.put("BDCDYH", "510504001004GB00080W00000012");
+		parm.put("BDCDYH", "510504001004GB00080W00000013");
 
 		String jsondata = JSON.toJSONString(parm);
 		jsondata = jsondata.replace("[", "").replace("]", "");
 
 		System.out.println(jsondata);
 
-		IndexResponse response = client.prepareIndex(indexName, typeName).setId("3")
-				.setSource(jsondata, XContentType.JSON).get();
+		IndexResponse response = client.prepareIndex(indexName, typeName, id).setSource(jsondata, XContentType.JSON)
+				.get();
 
 		while (response.isFragment()) {
 			System.out.println("写入成功");
@@ -192,7 +223,7 @@ public class ESUtils {
 	 * 
 	 * @throws IOException
 	 */
-	public static void writeDocumentByIndexRequest(TransportClient client, String indexName, String typeName)
+	public static void writeDocumentByIndexRequest(TransportClient client, String indexName, String typeName, String id)
 			throws IOException {
 
 		Map<String, String> parm = new HashMap<String, String>(1);
@@ -203,7 +234,7 @@ public class ESUtils {
 
 		System.out.println(parm);
 
-		IndexResponse response = client.index(new IndexRequest(indexName, typeName, "2").source(parm)).actionGet();
+		IndexResponse response = client.index(new IndexRequest(indexName, typeName, id).source(parm)).actionGet();
 
 		/*
 		 * by way one
@@ -226,8 +257,8 @@ public class ESUtils {
 	 * @param typeName
 	 * @throws IOException
 	 */
-	public static void UpdateDocumentByUpdateRequest(TransportClient client, String indexName, String typeName)
-			throws IOException {
+	public static void UpdateDocumentByUpdateRequest(TransportClient client, String indexName, String typeName,
+			String id) throws IOException {
 
 		Map<String, String> parm = new HashMap<String, String>(1);
 
@@ -237,7 +268,7 @@ public class ESUtils {
 
 		System.out.println(parm);
 
-		UpdateResponse response = client.update(new UpdateRequest(indexName, typeName, "0").doc(parm)).actionGet();
+		UpdateResponse response = client.update(new UpdateRequest(indexName, typeName, id).doc(parm)).actionGet();
 
 		System.out.println("修改成功,分片信息" + response.getShardInfo().toString());
 
@@ -251,8 +282,8 @@ public class ESUtils {
 	 * @param typeName
 	 * @throws IOException
 	 */
-	public static void UpdateDocumentByPrepareUpdate(TransportClient client, String indexName, String typeName)
-			throws IOException {
+	public static void UpdateDocumentByPrepareUpdate(TransportClient client, String indexName, String typeName,
+			String id) throws IOException {
 
 		Map<String, String> parm = new HashMap<String, String>(1);
 
@@ -262,9 +293,34 @@ public class ESUtils {
 
 		System.out.println(parm);
 
-		UpdateResponse response = client.prepareUpdate(indexName, typeName, "0").setDoc(parm, XContentType.JSON).get();
+		UpdateResponse response = client.prepareUpdate(indexName, typeName, id).setDoc(parm, XContentType.JSON).get();
 
 		System.out.println("修改成功,分片信息" + response.getShardInfo().toString());
+
+	}
+
+	/***
+	 * delete index by PrepareDelete
+	 * 
+	 * @param client
+	 * @param indexName
+	 * @param typeName
+	 * @throws IOException
+	 */
+	public static void deleteDocumentByPrepareDelete(TransportClient client, String indexName, String typeName,
+			String id) throws IOException {
+
+		Map<String, String> parm = new HashMap<String, String>(1);
+
+		parm.put("BDCDYH", "123");
+
+		// String jsondata = JSON.toJSONString(parm);
+
+		System.out.println(parm);
+
+		DeleteResponse response = client.prepareDelete(indexName, typeName, id).get();
+
+		System.out.println("删除成功,分片信息" + response.getShardInfo().toString());
 
 	}
 
@@ -288,12 +344,105 @@ public class ESUtils {
 	}
 
 	/**
-	 * 删除数据
+	 * delete by id
 	 * 
 	 * @throws UnknownHostException
 	 */
-	public static void deleteDocument(TransportClient client, String indexName, String typeName, String documentID)
-			throws UnknownHostException {
-		DeleteResponse response = client.prepareDelete(indexName, typeName, documentID).get();
+	public static void deleteDocumentByDeleteRequest(TransportClient client, String indexName, String typeName,
+			String documentID) throws UnknownHostException {
+		DeleteResponse response = client
+				.delete(new org.elasticsearch.action.delete.DeleteRequest(indexName, typeName, documentID)).actionGet();
+		System.out.println("delete success,docid is :" + response.getId());
 	}
+
+	/**
+	 * delete by query
+	 * 
+	 * @throws UnknownHostException*/
+	public static void deleteDocumentByQuery(TransportClient client, String indexName) throws UnknownHostException {
+		BulkByScrollResponse response = DeleteByQueryAction.INSTANCE.newRequestBuilder(client)
+				.filter(QueryBuilders.termQuery("postDate", "xiugaihou")).source(indexName).get();
+
+		long deleted = response.getDeleted();
+		System.out.println("delete cost :" + deleted);
+	}
+
+	/****
+	 * use BulkRequestBuilder batch execs
+	 * 
+	 * @throws UnknownHostException
+	 */
+	public static void useBulkRequestBuilder(TransportClient client, String indexName, String typeName)
+			throws UnknownHostException {
+		// Settings settings = Settings.builder().put("cluster.name",
+		// "es").build();
+		// TransportClient client = new PreBuiltTransportClient(settings);
+		// client.addTransportAddress(new
+		// InetSocketTransportAddress(InetAddress.getByName("10.10.6.6"),
+		// 9300));
+		BulkRequestBuilder bulkRequestBuilder = client.prepareBulk();
+
+		Map<String, Object> json = new HashMap();
+		json.put("BDCDYH", "1");
+		json.put("postDate", "2017-11-12 12:22:23");
+		Map<String, Object> json2 = new HashMap();
+		json2.put("BDCDYH", "2");
+		json2.put("postDate", "xiugaiqian");
+		Map<String, Object> json3 = new HashMap();
+		json3.put("BDCDYH", "2");
+		json3.put("postDate", "xiugaihou");
+		IndexRequestBuilder update1 = client.prepareIndex(indexName, typeName, "001").setSource(json);
+		IndexRequestBuilder update2 = client.prepareIndex(indexName, typeName, "002").setSource(json2);
+		UpdateRequestBuilder update3 = client.prepareUpdate(indexName, typeName, "002").setDocAsUpsert(true)
+				.setDoc(json3);
+		bulkRequestBuilder.add(update1);
+		bulkRequestBuilder.add(update2);
+		bulkRequestBuilder.add(update3);
+		// bulkRequestBuilder.add(update3);
+		BulkResponse bulkItemResponse = bulkRequestBuilder.execute().actionGet();
+		if (bulkItemResponse.hasFailures()) {
+			System.out.println(bulkItemResponse.buildFailureMessage());
+		}
+		client.close();
+	}
+
+	/***
+	 * if you bach process index and delete request you can use this way,not for
+	 * update
+	 * 
+	 * @param client
+	 * @param index
+	 * @param type
+	 */
+	public static void useBulkProcessor(TransportClient client, String index, String type) {
+		BulkProcessor bulkProcessor = BulkProcessor.builder(client, new BulkProcessor.Listener() {
+			public void beforeBulk(long executionId, BulkRequest request) {
+			}
+
+			public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
+				System.out.println("尝试插入：" + request.numberOfActions() + "条记录");
+			}
+
+			public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
+			}
+		}).setBulkSize(new ByteSizeValue(5, ByteSizeUnit.MB)).setBulkActions(10000)
+				.setFlushInterval(TimeValue.timeValueSeconds(3)).build();
+
+		Map<String, String> parm = new HashMap<String, String>(1);
+		parm.put("BDCDYH", "510504001004GB00080W00000013");
+
+		IndexRequest indexRequest = new IndexRequest(index, type, "003").source(parm);
+		bulkProcessor.add(indexRequest);
+
+		parm.put("BDCDYH", "510504001004GB00080W00000014");
+		IndexRequest indexRequest1 = new IndexRequest(index, type, "004").source(parm);
+		bulkProcessor.add(indexRequest1);
+
+		// error : bulkProcessor dont exec updateRequest
+		// parm.put("BDCDYH", "510504001004GB00080W00000014");
+		// new UpdateRequest(index, type, "001").doc(parm);
+		// bulkProcessor.add(UpdateRequest);
+
+	}
+
 }
